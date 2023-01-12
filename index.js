44e233b80bf5eaca30380d51ec6673e200dfe4fe
vrmcardoso <vrmcardoso@gmail.com>
2023-01-12 13:15:50 +0000

    Created the frontend

---
+++ index.js
@@ -0,0 +1,16 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y"
+,"Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1"
+, "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]"
+,",","|",":",";","<",">",".","?","/"];
+
+function generatePasswords() {
+  let passwordOneArray = []
+  let passwordTwoArray = []
+  for (i = 1; i = 15; i++) {
+    let characterA = characters[Math.floor(Math.random() * characters.length)]
+    let characterB = characters[Math.floor(Math.random() * characters.length)]
+    passwordOneArray.push(characterA)
+    passwordTwoArray.push(characterB)
+  }
+  
+}
