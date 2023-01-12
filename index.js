cbc73672f72c36b00231ad3dea0d431de8be09ee
vrmcardoso <vrmcardoso@gmail.com>
2023-01-12 13:41:27 +0000

    Version 1 complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y"
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y"
 ,"Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1"
 , "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]"
 ,",","|",":",";","<",">",".","?","/"];
@@ -6,11 +6,15 @@ const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"
 function generatePasswords() {
   let passwordOneArray = []
   let passwordTwoArray = []
-  for (i = 1; i = 15; i++) {
+  for (i = 0; i < 9; i++) {
     let characterA = characters[Math.floor(Math.random() * characters.length)]
     let characterB = characters[Math.floor(Math.random() * characters.length)]
     passwordOneArray.push(characterA)
     passwordTwoArray.push(characterB)
+    console.log(passwordOneArray)
   }
-  
+  let passOne = document.getElementById("pass-one")
+  let passTwo = document.getElementById("pass-two")
+  passOne.textContent = passwordOneArray.join("")
+  passTwo.textContent = passwordTwoArray.join("")
 }
